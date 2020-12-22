4e83e474a5177f1d6f9bd33b9d0bd1f2e3f15f9e
HussainArif <hussainarifkl@gmail.com>
2020-12-22 17:41:10 +0500

    Removed moment and added timerDone event

--- src/Timer.js
+++ src/Timer.js
@@ -1,20 +1,36 @@
 import React, { useEffect, useState } from "react";
-import moment from "moment";
 import Duration from "luxon/src/duration.js";
 
 function Timer() {
-  const [timerLength, setTimerLength] = useState(23.5 * 60);
+  const [timerLength, setTimerLength] = useState(10);
   const [timerOn, setTimerOn] = useState(false);
   const [timerDone, setTimerDone] = useState(false);
 
   const [sessionType, setSessionType] = useState("Work");
   const [sessionNumber, setSessionNumber] = useState(0);
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (timerOn) {
+        setTimerLength((timerLength) => timerLength - 1);
+      }
+    }, 200);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [timerOn]);
+
+  useEffect(() => {
+    if (timerLength === 0) {
+      setTimerOn(false);
+      setTimerDone(false);
+    }
+  }, [timerLength]);
   return (
     <>
       <p>
-        Duration:
-        {Duration.fromObject({ seconds: timerLength }).toFormat("mm : ss")}
+        Duration:{" "}
+        {Duration.fromObject({ seconds: timerLength }).toFormat("mm:ss")}
       </p>
       <button onClick={() => setTimerOn(!timerOn)}>
         {timerOn ? "Pause" : "Run"}
